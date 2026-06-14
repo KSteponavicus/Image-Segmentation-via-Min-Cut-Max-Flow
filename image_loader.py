5482154dd7d79e123383925fcd0cc2a6a7c5de83
KSteponavicius <kipras.steponavicius@psl.eu>
2026-06-14 14:50:42 +0300

    Updated Image_loader.py

--- image_loader.py
+++ image_loader.py
@@ -4,41 +4,31 @@ import math
 image = Image.open("small_test.jpg")
 image2 = Image.open("dog.jpg")
 
+LAMBDA = 100
+
 def image_to_graph(image, sigma = 25):
 
-    di = [-1, 0, 1, 0]
-    dj = [0, 1, 0, -1]
-
     width, height = image.size
-
-
+    
     graph_caps = {}
         
-    for i in range(width):
-        for j in range(height):
+    for x in range(width):
+        for y in range(height):
             # For grayscale images all channels have the same value
-            Ip, _, _ = image.getpixel((i, j))
+            Ip, _, _ = image.getpixel((x, y))
 
-            # Converting to the graph nodes
-            p = j * width + i + 1
-            #  print(f"({i},{j}) corresponds to {p}")
+            for dx, dy in [(1,0), (0,1)]:
+                nx = x + dx
+                ny = y + dy
 
-            for k in range(4):
-                i2 = i + di[k]
-                j2 = j + dj[k]
+                if 0 <= nx < width and 0 <= ny < height:
+                    Iq, _, _ = image.getpixel((nx, ny))
 
-                if i2 < 0 or i2 >= width:
-                    continue
+                    w = int(LAMBDA * math.exp(-((Ip-Iq)**2)/(2*sigma**2)))
 
-                if j2 < 0 or j2 >= height:
-                    continue
-
-                Iq, _, _ = image.getpixel((i2, j2))
-                q = j2 * width + i2 + 1
-
-                w = math.exp(-((Ip-Iq)**2)/(2*sigma**2))
-                graph_caps[(p,q)] = w 
-                #  print(f"Weight between ({i},{j}) and ({i2},{j2}) is {w}")
+                    graph_caps[(x,y), (nx, ny)] = w 
+                    graph_caps[(nx, ny), (x,y)] = w
+                
 
     return graph_caps
 
